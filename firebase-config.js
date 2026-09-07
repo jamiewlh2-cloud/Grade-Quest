d5c50490992f09069f38ce176bc64df35decdf11
jamiewlh2-cloud <jamiewlh2@gmail.com>
2026-09-07 15:40:17 -0400

    added firebase config

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.GRADEQUEST_FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBcBGGzOhi21Nvndh9wccEm6Grt9EEudBo",
+    authDomain: "school-pwa-a4fe2.firebaseapp.com",
+    projectId: "school-pwa-a4fe2",
+    storageBucket: "school-pwa-a4fe2.firebasestorage.app",
+    messagingSenderId: "69635254035",
+    appId: "1:69635254035:web:a06a2da9dcb51697bd5ce6"
+};
